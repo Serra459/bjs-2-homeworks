0c3174f3762127ef552097ac02fa7d50a2354482
Nastya <anistat10@yandex.ru>
2025-02-26 21:16:42 +0300

    Edits

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,23 +3,12 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let discriminant = Math.pow(b, 2) - 4 * a * c;
-  let solution = 0;
-  let solutionSecond = 0;
-  if (discriminant < 0) {
-    let arr = [];
-  }
-  else if (discriminant == 0) {
-    solution = - b / (2 * a);
-    arr.push(solution);
-    // let arr = [(- b / (2 * a))];
-  } else {
-    // solution = (-b + Math.sqrt(d) )/(2*a);
-    // solutionSecond = (-b - Math.sqrt(d) )/(2*a);
-    // arr.push(solution, solutionSecond);
-    // // solution = (-b - Math.sqrt(d) )/(2*a);
-    // // arr.push(solution);
+  if (discriminant > 0) {
     arr = [(-b + Math.sqrt(discriminant) )/(2*a), (-b - Math.sqrt(discriminant) )/(2*a)];
   }
+  else if (discriminant == 0) {
+    arr = [- b / (2 * a)];
+  } 
 
   return arr;
 }
